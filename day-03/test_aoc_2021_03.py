3cb968076dc57f63d6058608a347d92fe50ca87b
Malanius Privierre <malaniusprivierre@gmail.com>
2021-12-05 14:00:00 +0100

    (Day 03) Add test cases for bits filtering

--- day-03/test_aoc_2021_03.py
+++ day-03/test_aoc_2021_03.py
@@ -2,6 +2,8 @@ import pathlib
 import pytest
 import aoc_2021_03 as solver
 
+import pandas as pd
+
 PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
@@ -37,3 +39,16 @@ def test_part1(example):
 def test_part2_example2(example):
     """Test part 2 on example input"""
     assert solver.part2(example) == 230
+
+
+def test_filter_out_least_common_bits(example):
+    df = pd.DataFrame(example)
+    filtered = solver.filter_out_least_common_bits(df)
+    bits = filtered.values.tolist()[0]
+    assert bits == ['1','0', '1', '1', '1']
+
+def test_filter_out_most_common_bits(example):
+    df = pd.DataFrame(example)
+    filtered = solver.filter_out_most_common_bits(df)
+    bits = filtered.values.tolist()[0]
+    assert bits == ['0','1', '0', '1', '0']
