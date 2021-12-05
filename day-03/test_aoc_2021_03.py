5b9faba69bd905fb7976c8c5c7cfac4acc1f7cdc
Malanius Privierre <malaniusprivierre@gmail.com>
2021-12-05 13:40:20 +0100

    (Day 03) Add test case for part 2

--- day-03/test_aoc_2021_03.py
+++ day-03/test_aoc_2021_03.py
@@ -6,20 +6,14 @@ PUZZLE_DIR = pathlib.Path(__file__).parent
 
 
 @pytest.fixture
-def example1():
-    puzzle_input = (PUZZLE_DIR / "example-1.txt").read_text().strip()
+def example():
+    puzzle_input = (PUZZLE_DIR / "example.txt").read_text().strip()
     return solver.parse(puzzle_input)
 
 
-@pytest.fixture
-def example2():
-    puzzle_input = (PUZZLE_DIR / "example-2.txt").read_text().strip()
-    return solver.parse(puzzle_input)
-
-
-def test_parse_example1(example1):
+def test_parse_example1(example):
     """Test that input is parsed properly"""
-    assert example1 == [
+    assert example == [
         ['0', '0', '1', '0', '0'],
         ['1', '1', '1', '1', '0'],
         ['1', '0', '1', '1', '0'],
@@ -35,12 +29,11 @@ def test_parse_example1(example1):
     ]
 
 
-def test_part1_example1(example1):
+def test_part1(example):
     """Test part 1 on example input"""
-    assert solver.part1(example1) == 198
+    assert solver.part1(example) == 198
 
 
-@pytest.mark.skip(reason="Not implemented")
-def test_part2_example2(example2):
+def test_part2_example2(example):
     """Test part 2 on example input"""
-    assert solver.part2(example2) == ...
+    assert solver.part2(example) == 230
