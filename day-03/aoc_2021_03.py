28f8081b3e7ed8e37102650af82210ce54ba6bf6
Malanius Privierre <malaniusprivierre@gmail.com>
2021-12-05 10:32:34 +0100

    (Day 03) Parse puzzle input

---
+++ day-03/aoc_2021_03.py
@@ -0,0 +1,31 @@
+import pathlib
+
+PUZZLE_DIR = pathlib.Path(__file__).parent
+
+
+def parse(puzzle_input: str):
+    """Parse input"""
+    lines = puzzle_input.splitlines()
+    data = [list(line) for line in lines]
+    return data
+
+def part1(data):
+    """Solve part 1"""
+
+
+def part2(data):
+    """Solve part 2"""
+
+
+def solve(puzzle_input):
+    """Solve the puzzle for the given input"""
+    data = parse(puzzle_input)
+    solution1 = part1(data)
+    solution2 = part2(data)
+    return solution1, solution2
+
+
+if __name__ == "__main__":
+    puzzle_input = (PUZZLE_DIR / "data.txt").read_text().strip()
+    solutions = solve(puzzle_input)
+    print("\n".join(str(solution) for solution in solutions))
